2a77ce2b35c461eb38017df70724b9b043c2c1bb
devyako <yakhyoismoildjonov@gmail.com>
2020-05-19 17:34:03 +0500

    update

---
+++ src/shared/components/Navigation/MainHeader.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./MainHeader.css";
+
+const MainHeader = (props) => {
+  return <header className="main-header"></header>;
+};
+
+export default MainHeader;
+ 
--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./UserItem.css";
 import Avatar from "../../shared/components/UIElements/Avatar";
+import Card from "../../shared/components/UIElements/Card";
 
 const UserItem = (props) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
+      <Card className="user-item__content">
         <Link to={`/${props.id}/paces`}>
           <div className="user-item__image">
             <Avatar image={props.image} alt={props.name} />
@@ -18,7 +19,7 @@ const UserItem = (props) => {
             </h3>
           </div>
         </Link>
-      </div>
+      </Card>
     </li>
   );
 };
