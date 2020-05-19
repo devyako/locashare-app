6c1110fcb26485c90060ff1cc526ac12251c44ee
devyako <yakhyoismoildjonov@gmail.com>
2020-05-19 18:07:56 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import User from "./user/pages/Users";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import NewPlaces from "./places/pages/NewPlace";
 
 const App = () => {
   return (
     <Router>
-      <Route path="/" exact component={User} />
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact component={User} />
+          <Route path="/places/new" exact component={NewPlaces} />
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 };
--- src/shared/components/Navigation/MainHeader.js
+++ src/shared/components/Navigation/MainHeader.js
@@ -3,8 +3,7 @@ import React from "react";
 import "./MainHeader.css";
 
 const MainHeader = (props) => {
-  return <header className="main-header"></header>;
+  return <header className="main-header">{props.childeren}</header>;
 };
 
 export default MainHeader;
- 
