fb9d1812d473c563333d0866c93db6a6f7bd4e6c
devyako <yakhyoismoildjonov@gmail.com>
2020-05-20 23:42:20 +0500

    update

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -13,7 +13,7 @@ const MainNavigation = (props) => {
         <span />
       </button>
       <h1 className="main-navigation__title">
-        <Link to="/">YourPlaces</Link>
+        <Link to="/">Your Places</Link>
       </h1>
       <na>...</na>
     </MainHeader>
