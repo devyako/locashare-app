bfb96f874e5186a35c4b1e36d87ff8516f62c601
devyako <yakhyoismoildjonov@gmail.com>
2020-05-21 16:28:06 +0500

    update

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,22 +1,45 @@
-import React from "react";
+import React, { useState, Fragment } from "react";
 import { Link } from "react-router-dom";
 
 import "./MainNavigation.css";
 import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
 
 const MainNavigation = (props) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawer = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawer = () => {
+    setDrawerIsOpen(false);
+  };
+
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">Your Places</Link>
-      </h1>
-      <na>...</na>
-    </MainHeader>
+    <Fragment>
+      {drawerIsOpen && (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      )}
+      <MainHeader>
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">Your Places</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </Fragment>
   );
 };
 
--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -7,19 +7,17 @@ import Card from "../../shared/components/UIElements/Card";
 const UserItem = (props) => {
   return (
     <li className="user-item">
-      <Card className="user-item__content">
-        <Link to={`/${props.id}/paces`}>
-          <div className="user-item__image">
-            <Avatar image={props.image} alt={props.name} />
-          </div>
-          <div className="user-item__info">
-            <h2>{props.name}</h2>
-            <h3>
-              {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
-            </h3>
-          </div>
-        </Link>
-      </Card>
+      <Link to={`/${props.id}/paces`}>
+        <div className="user-item__image">
+          <Avatar image={props.image} alt={props.name} />
+        </div>
+        <div className="user-item__info">
+          <h2>{props.name}</h2>
+          <h3>
+            {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
+          </h3>
+        </div>
+      </Link>
     </li>
   );
 };
