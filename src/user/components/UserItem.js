ff1693f3d4c5197261e15c7cbe9f1f47d9b86997
devyako <yakhyoismoildjonov@gmail.com>
2020-05-24 16:57:46 +0500

    update

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./UserItem.css";
 import Avatar from "../../shared/components/UIElements/Avatar";
-import Card from "../../shared/components/UIElements/Card";
 
 const UserItem = (props) => {
   return (
