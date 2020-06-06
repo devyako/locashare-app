800dc4b67cbf65e361bfc26b510e099bde0033f3
devyako <yakhyoismoildjonov@gmail.com>
2020-06-06 14:19:17 +0500

    update

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,11 +1,20 @@
 import React from "react";
 
+import "./NewPlace.css";
+import Input from "../../shared/components/formElements/Input";
+
 const NewPlace = () => {
-  return <form className="place-form">
-
-  	
-
-  </form>;
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        label="title"
+        type="text"
+        validators={[]}
+        errorText="Please enter a valid title!"
+      />
+    </form>
+  );
 };
 
 export default NewPlace;
