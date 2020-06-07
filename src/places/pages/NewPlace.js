8d0741e2cb0ea2bffe2ca2edaeaa606ad10e7881
devyako <yakhyoismoildjonov@gmail.com>
2020-06-07 19:36:21 +0500

    update

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import "./NewPlace.css";
-import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
 import Input from "../../shared/components/formElements/Input";
 
 const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
   return (
     <form className="place-form">
       <Input
+        id="title"
         element="input"
         label="title"
         type="text"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title!"
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 charecters!)"
+        onInput={descriptionInputHandler}
       />
     </form>
   );
